ed1e32e73f9c9a4caa2e2372f420f9654e334535
mreshboboyev <mreshboboyev@gmail.com>
2023-12-17 21:52:49 +0500

    StudentRepository.cs created : inherits from IStudentRepository.cs, Repository<Student> and methods implemented

---
+++ BulkySelf.DataAccess/Repository/StudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkySelf.DataAccess.Data;
+using BulkySelf.DataAccess.Repository.IRepository;
+using BulkySelf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkySelf.DataAccess.Repository
+{
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public StudentRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Student obj)
+        {
+            _db.Students.Update(obj);
+        }
+    }
+}
