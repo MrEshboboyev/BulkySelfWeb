926649fc61830571bda94d35d74fcb6d07f7a888
mreshboboyev <mreshboboyev@gmail.com>
2023-12-17 21:44:42 +0500

    IStudentRepository.cs created in IRepository folder and inherits from IRepository.cs, (two methods added: (Update), (Save))

---
+++ BulkySelf.DataAccess/Repository/IRepository/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkySelf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkySelf.DataAccess.Repository.IRepository
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        void Update(Student obj);
+        void Save();
+    }
+}
