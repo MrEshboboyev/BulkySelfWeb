b79c4498ce6a61c6a84245c01a64ab09926d7481
mreshboboyev <mreshboboyev@gmail.com>
2023-12-17 22:10:28 +0500

    IUnitOfWork.cs created in IRepository folder and added this file : IStudentRepository and Save() method

---
+++ BulkySelf.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkySelf.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IStudentRepository StudentRepository { get; }
+        void Save();
+    }
+}
