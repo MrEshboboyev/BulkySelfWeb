b50a64e47ce9766f87598f5b20d642a1ae7d666f
mreshboboyev <mreshboboyev@gmail.com>
2023-12-26 00:16:29 +0500

    Buttons added to product.js and 'Delete' action and view removed project, 'Delete' method in API CALLS region

--- BulkySelfWeb/wwwroot/js/product.js
+++ BulkySelfWeb/wwwroot/js/product.js
@@ -4,6 +4,28 @@ $(document).ready(function () {
     loadDataTable();
 })
 
+//function loadDataTable() {
+//    dataTable = $('#tblData').DataTable({
+//        "ajax": { url: '/admin/product/getall' },
+//        "columns": [
+//            { data: "title", "width": "25%" },
+//            { data: "isbn", "width": "15%" },
+//            { data: "listPrice", "width": "10%" },
+//            { data: "author", "width": "20%" },
+//            { data: "student.name", "width": "10%" },
+//            {
+//                data: 'id',
+//                "render": function (data) {
+//                    return `<div class="w-75 btn-group" role="group">
+//                        <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+//                        <a OnClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+//                    </div>`
+//                }
+//            }
+//        ]
+//    });
+//}
+
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
@@ -12,7 +34,16 @@ function loadDataTable() {
             { data: "isbn", "width": "15%" },
             { data: "listPrice", "width": "10%" },
             { data: "author", "width": "20%" },
-            { data: "student.name", "width": "15%" },
+            { data: "student.name", "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                        <a href="/admin/product/delete/${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                }
+            }
         ]
     });
 }
