1ed09ec786a58be296c1e99a2efada6ab68fa016
mreshboboyev <mreshboboyev@gmail.com>
2023-12-25 19:27:53 +0500

    Saving the uploading image in "images\product" folder

--- BulkySelfWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkySelfWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkySelf.DataAccess.Repository.IRepository;
 using BulkySelf.Models;
 using BulkySelf.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -9,9 +10,11 @@ namespace BulkySelfWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -55,6 +58,19 @@ namespace BulkySelfWeb.Areas.Admin.Controllers
             {
                 if(productVM.Product.Id == 0)
                 {
+                    string wwwRootPath = _webHostEnvironment.WebRootPath;
+                    if (file != null)
+                    {
+                        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                        string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                        using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                        {
+                            file.CopyTo(fileStream);
+                        }
+
+                        productVM.Product.ImageUrl = @"\images\product" + fileName;
+                    }
                     _unitOfWork.Product.Add(productVM.Product);
                 }
                 else
