5df73b5c9f57ccea2aad704f1bb1e6e3ba959ef5
mreshboboyev <mreshboboyev@gmail.com>
2023-12-25 15:32:57 +0500

    Connection with Foreign Key : 'Product' and 'Student', Updating Database after migrations and Seeding Data (with StudentId) [conflict occured and fixed  with changing DB]

---
+++ BulkySelf.DataAccess/Migrations/20231225102911_AddForeignKeyForStudentProductRelation.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkySelf.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyForStudentProductRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StudentId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StudentId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "StudentId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "StudentId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "StudentId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "StudentId",
+                value: 3);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_StudentId",
+                table: "Products",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Students_StudentId",
+                table: "Products",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Students_StudentId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_StudentId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Products");
+        }
+    }
+}
--- BulkySelf.Models/Product.cs
+++ BulkySelf.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,5 +40,9 @@ namespace BulkySelf.Models
         [DisplayName("Price for 100 + ")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int StudentId { get; set; }
+        [ForeignKey("StudentId")]
+        public Student Student { get; set; }
     }
 }
