6918922293968a0487f2cb7e122d2de338de2dfc
mreshboboyev <mreshboboyev@gmail.com>
2023-12-26 11:33:33 +0500

    Index() action in HomeController returning Products with UnitOfWork

--- BulkySelfWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkySelfWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BulkySelf.DataAccess.Repository.IRepository;
 using BulkySelf.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace BulkySelfWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Student");
+            return View(productList);
         }
 
         public IActionResult Privacy()
